9c49b08186af6d2af9b52671393e2e0c11b7f97c
hasanshanto <hasanshanto01@gmail.com>
2024-01-18 17:27:42 +0600

    repo test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  );
+}
+
+export default App;
